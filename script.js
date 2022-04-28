2d4d61f9e28451cfea85e5b6a99074a5f5689f89
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-04-28 13:59:46 -0600

    Create html, css and javascript files
    Create the layout of the webpage and layout
    of elements with combination of flexbox and
    grid, add styling and animation to cards
    
    Create the basic logic for the creation
    of new entries and removal of said elements,
    change of status on the object of said element.
    
    Changes are save on local storage so data is
    persistent and won't be overwritten on the reload
    of the webpage.
    
    Todo, Mobile responsiveness

---
+++ script.js
@@ -0,0 +1,158 @@
+//Queried Selectors
+//Get locally stored json and parse, if doesnt exist create empty array
+const myLibrary = JSON.parse(localStorage.getItem('library')) || [];
+const cardsContainer = document.querySelector('main')
+const addBookButton = document.querySelector('.newBook') 
+const form = document.querySelector('form')
+
+//Submitting form event listener
+form.addEventListener('submit', validateForm)
+
+//Object constructor
+function Book(title,author,pages,status,color){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.color = color
+}
+Book.prototype.info = function(){
+    `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
+}
+
+function addBookToLibrary(title='Missing Info',author = 'Missing Info',pages ='Missing Info',status = false,color) {
+    // Creates new Book object and appends its to the library array
+    let newBook = new Book(title,author,pages,status,color);
+    myLibrary.push(newBook)
+
+    //Store the array of books on the local storage as a JSON
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+//Display of Current myLibrary array when invoked
+function populateLibrary(){
+    //Clears current displayed elements
+    clear()
+
+    //Iterate through library and each book card
+    myLibrary.forEach(book=>{
+        //Create Card Element and sets its color theme
+        let card = document.createElement('div');
+        card.classList.add(`${book.color}`, 'book')
+
+        //Cover of the card and appending of classes
+        let cover = document.createElement('div')
+        cover.classList.add('cover')
+        let header = document.createElement('h1')
+        header.textContent = book.title
+        cover.appendChild(header)
+        card.appendChild(cover)
+
+        //Pages for animation on hover
+        for(let i = 0;i<5;i++){
+            let page = document.createElement('div')
+            page.classList.add('page')
+            card.appendChild(page)
+        }
+
+        // Create content of the book card by setting the current book
+        // properties to its corresponding element
+        
+        let lastPage = document.createElement('div')
+        lastPage.classList.add('last-page')
+        let title = document.createElement('h2')
+        title.textContent = book.title
+        let author = document.createElement('h2')
+        author.textContent = book.author
+        let pages = document.createElement('h2')
+        pages.textContent = book.pages
+        let status = document.createElement('h2')
+        status.textContent = book.status
+        
+
+        //Creating and Appending options for cards
+        let options = document.createElement('div')
+        options.classList.add('options')
+        let button1 = document.createElement('button')
+        button1.setAttribute('type', 'button')
+        button1.classList.add('status')
+        if(book.status){
+            button1.classList.add('readed')
+            button1.textContent = 'Readed'
+        }
+        else{
+            button1.classList.add('not-readed')
+            button1.textContent = 'Not Readed'
+        }
+        button1.addEventListener('click', toggleStatus)
+
+        let button2 = document.createElement('button')
+        button2.setAttribute('type', 'button')
+        button2.classList.add('delete')
+        button2.textContent = 'Delete'
+        button2.addEventListener('click', deleteEntry)
+
+        options.appendChild(button1)
+        options.appendChild(button2)
+
+        
+        //Appending content to last page
+        lastPage.appendChild(title)
+        lastPage.appendChild(author)
+        lastPage.appendChild(pages)
+        lastPage.appendChild(options)
+        card.appendChild(lastPage)
+
+        
+        //BackCover
+        let backCover = document.createElement('div')
+        backCover.classList.add('back-cover')
+        card.appendChild(backCover)
+
+        //Append node to main container
+        cardsContainer.appendChild(card)
+    })
+}
+
+//Clears current displayed elements when invoked
+function clear(){
+    while (cardsContainer.firstChild) {
+        cardsContainer.removeChild(cardsContainer.lastChild);
+    }
+}
+
+//Triggered when form is submitted, parses data from the form and calls
+//addBookToLibrary function
+function validateForm(event){
+    event.preventDefault()
+
+    let data = Object.fromEntries(new FormData(event.target).entries());
+    addBookToLibrary(data.title, data.author, data.numPages, data.status, data['color-cover'])
+    populateLibrary()
+    //Clears form data anew
+    event.target.reset()
+}
+
+//toggles status property of the selected book
+function toggleStatus(event){
+    let index = [...event.target.parentNode.parentNode.parentNode.parentNode.children].indexOf(event.target.parentNode.parentNode.parentNode)
+    myLibrary[index].status = myLibrary[index].status ? false:true;
+    //Displays updated myLibrary
+    populateLibrary()
+
+    //Save changes on the local storage
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+//deletes book of the library 
+function deleteEntry(event){
+    let index = [...event.target.parentNode.parentNode.parentNode.parentNode.children].indexOf(event.target.parentNode.parentNode.parentNode)
+    myLibrary.splice(index, 1)
+    //Displays updated myLibrary
+    populateLibrary()
+    
+    //Save changes on the local storage
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+
+document.addEventListener('DOMContentLoaded', populateLibrary)
